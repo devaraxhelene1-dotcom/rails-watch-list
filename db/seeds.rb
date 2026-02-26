19efd3dcbebec87d1b3a4855d23a47c1988c9d7e
helene riverieulx de varax <devarax.helene1@gmail.com>
2026-02-26 16:36:45 +0100

    rails watch list updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,50 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+Bookmark.destroy_all
+List.destroy_all
+Movie.destroy_all
+
+puts "Movies creating..."
+wonder_woman = Movie.create(
+  title: "Wonder Woman 1984",
+  overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s",
+  poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg",
+  rating: 6.9
+)
+
+shawshank = Movie.create(
+  title: "The Shawshank Redemption",
+  overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison",
+  poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+  rating: 8.7
+)
+
+titanic = Movie.create(
+  title: "Titanic",
+  overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.",
+  poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg",
+  rating: 7.9
+)
+
+oceans_eight = Movie.create(
+  title: "Ocean's Eight",
+  overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.",
+  poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg",
+  rating: 7.0
+)
+puts "Movies created..."
+
+puts "Lists creating..."
+drama_list = List.create(name: "Drama")
+classics_list = List.create(name: "Classics")
+puts "Lists created..."
+
+puts "Bookmarks creating..."
+Bookmark.create(movie: wonder_woman, list: classics_list, comment: "Action épique !")
+Bookmark.create(movie: shawshank, list: drama_list, comment: "Émotion garantie")
+Bookmark.create(movie: titanic, list: drama_list, comment: "Histoire émouvante")
+Bookmark.create(movie: oceans_eight, list: classics_list, comment: "Super film divertissant")
+puts "Bookmarks created..."
