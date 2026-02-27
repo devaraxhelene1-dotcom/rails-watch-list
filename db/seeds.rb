32e321aab02da6e48f0f329c9bb7fe2f323abee0
helene riverieulx de varax <devarax.helene1@gmail.com>
2026-02-27 15:47:49 +0100

    update seed using imdb api

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,56 +1,72 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# # This file should ensure the existence of records required to run the application in every environment (production,
+# # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# #
+# # Example:
+# #
+# #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+# #     MovieGenre.find_or_create_by!(name: genre_name)
+# #   end
 
 puts "Cleaning database..."
 Bookmark.destroy_all
 List.destroy_all
 Movie.destroy_all
 
-puts "Movies creating..."
-wonder_woman = Movie.create(
-  title: "Wonder Woman 1984",
-  overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s",
-  poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg",
-  rating: 6.9
-)
+# puts "Movies creating..."
+# wonder_woman = Movie.create(
+#   title: "Wonder Woman 1984",
+#   overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s",
+#   poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg",
+#   rating: 6.9
+# )
 
-shawshank = Movie.create(
-  title: "The Shawshank Redemption",
-  overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison",
-  poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
-  rating: 8.7
-)
+# shawshank = Movie.create(
+#   title: "The Shawshank Redemption",
+#   overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison",
+#   poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+#   rating: 8.7
+# )
 
-titanic = Movie.create(
-  title: "Titanic",
-  overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.",
-  poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg",
-  rating: 7.9
-)
+# titanic = Movie.create(
+#   title: "Titanic",
+#   overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.",
+#   poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg",
+#   rating: 7.9
+# )
 
-oceans_eight = Movie.create(
-  title: "Ocean's Eight",
-  overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.",
-  poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg",
-  rating: 7.0
-)
-puts "Movies created..."
+# oceans_eight = Movie.create(
+#   title: "Ocean's Eight",
+#   overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.",
+#   poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg",
+#   rating: 7.0
+# )
+# puts "Movies created..."
 
-puts "Lists creating..."
-drama_list = List.create(name: "Drama")
-classics_list = List.create(name: "Classics")
-puts "Lists created..."
+# puts "Lists creating..."
+# drama_list = List.create(name: "Drama")
+# classics_list = List.create(name: "Classics")
+# puts "Lists created..."
 
-puts "Bookmarks creating..."
-Bookmark.create(movie: wonder_woman, list: classics_list, comment: "Action épique !")
-Bookmark.create(movie: shawshank, list: drama_list, comment: "Émotion garantie")
-Bookmark.create(movie: titanic, list: drama_list, comment: "Histoire émouvante")
-Bookmark.create(movie: oceans_eight, list: classics_list, comment: "Super film divertissant")
-puts "Bookmarks created..."
+# puts "Bookmarks creating..."
+# Bookmark.create(movie: wonder_woman, list: classics_list, comment: "Action épique !")
+# Bookmark.create(movie: shawshank, list: drama_list, comment: "Émotion garantie")
+# Bookmark.create(movie: titanic, list: drama_list, comment: "Histoire émouvante")
+# Bookmark.create(movie: oceans_eight, list: classics_list, comment: "Super film divertissant")
+# puts "Bookmarks created..."
+
+require 'open-uri'
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+json = URI.open(url).read
+movies = JSON.parse(json)["results"]
+
+movies.each do |movie|
+  title = movie["title"]
+  overview = movie["overview"]
+  poster_url = "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}"
+  rating = movie["vote_average"]
+
+  Movie.create!(title: title, overview: overview, poster_url: poster_url, rating: rating)
+  puts "#{Movie.all.last.title} created !"
+end
